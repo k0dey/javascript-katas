84bfb0c3b02d57bb9e229a8aa9c5df3cab8a034e
Kerry Parsons <kerryparsons1204@gmail.com>
2020-06-15 14:29:03 +0100

    solved square kata

--- square.js
+++ square.js
@@ -8,3 +8,35 @@ Given an integral number, determine if it's a square number:
  * isSquare(4) = true
  * isSquare(7) = false
  */
+
+
+ function isSquare(num) {
+     return Number.isInteger(Math.sqrt(num))
+ }
+
+// PURE MATH WAY
+
+// function isSquare(num) {
+//    let digitalRoot = 1 + (num - 1) % 9;
+//    //  ^ math shortcut for digital root, the remainder of any num
+//    //  divided by 9 is the digital root of the number
+    
+//    //  18 % 9 = 0 (0 = 9 with digital root)
+//    //  to get 9 rather than 0 we do
+//    //  18 - 1 which equals 17, modulo 9 equals 8. + 1 equals 9.
+
+//     return (digitalRoot === 1 ||
+//             digitalRoot === 4 ||
+//             digitalRoot === 7 ||
+//             digitalRoot === 9);
+
+//     // a perfect square's digital root will always be 1, 4, 7 or 9
+//     // it cannot be 2, 3, 5, 6 or 8
+
+//    }
+   console.log(isSquare(20)) // false
+   console.log(isSquare(9))   // true
+   console.log(isSquare(16))   // true
+   console.log(isSquare(64))    // true
+   console.log(isSquare(36))    // true
+   console.log(isSquare(244))   // false
